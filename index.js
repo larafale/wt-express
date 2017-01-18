c0185e9e89c828e2d77464dd7763dfddbf81b230
Louis Grellet <louis.grellet@gmail.com>
2017-01-18 22:50:19 +0100

    express api

---
+++ index.js
@@ -0,0 +1,42 @@
+"use latest";
+
+const app = new (require('express'))();
+const wt = require('webtask-tools');
+
+const err = (err, statusCode = 500) => ({
+  statusCode,
+  err: (err && err.message) || err || 'oups !'
+})
+
+const ok = (item, statusCode = 200) => ({
+  statusCode,
+  item
+})
+
+app.use((require('body-parser')).json());
+app.use((req, res, next) => {
+  res.err = (err, statusCode) => res.json(err(err, statusCode));
+  res.ok = (item, statusCode) => res.json(ok(item, statusCode));
+  next();
+})
+
+app.get('/', function (req, res) {
+  res.ok({app: 'running'});
+});
+
+app.get('/stars', function (req, res) {
+  req.webtaskContext.storage.get((err, data) => {
+    if(err) return res.err(err);
+    data = data || {counter: 1}
+    data.counter++;
+    req.webtaskContext.storage.set(data, (err) => res.ok(data))
+  })
+});
+
+module.exports = wt.fromExpress(app).auth0({
+  exclude : ['/'],
+  loginError: (error, ctx, req, res, baseUrl) => {
+    res.writeHead(401, { 'Content-Type': 'application/json'});
+    res.end(JSON.stringify(err('unauthorized', 401)));
+  }
+});
