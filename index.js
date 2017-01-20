05ef4370c1362a5487f8f98d88029b5fbfa4eb05
louis <louis.grellet@gmail.com>
2017-01-20 15:40:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,29 @@
-"use latest";
+"use latest"
 
-const app = new (require('express'))();
-const wt = require('webtask-tools');
+const app = new (require('express'))()
 
-const err = (err, statusCode = 500) => ({
-  statusCode,
-  err: (err && err.message) || err || 'oups ! try again.'
-})
 
-const ok = (item, statusCode = 200) => ({
-  statusCode,
-  item
-})
-
-app.use((require('body-parser')).json());
 app.use((req, res, next) => {
-  res.err = (err, statusCode) => res.json(err(err, statusCode));
-  res.ok = (item, statusCode) => res.json(ok(item, statusCode));
-  next();
+  res.err = (err, statusCode = 500) => res.json({ statusCode, err: (err && err.message) || err || 'oups ! try again.' })
+  res.ok = (item, statusCode = 200) => res.json({ statusCode, item })
+  next()
 })
 
-app.get('/', function (req, res) {
-  res.ok({app: 'rocking'});
-});
+app.get('/', (req, res) => res.ok({ app: 'running' }))
 
-app.get('/stars', function (req, res) {
+app.get('/stars', (req, res) => {
   req.webtaskContext.storage.get((err, data) => {
-    if(err) return res.err(err);
+    if(err) return res.err(err)
     data = data || {counter: 1}
-    data.counter++;
+    data.counter++
     req.webtaskContext.storage.set(data, (err) => res.ok(data))
   })
-});
+})
 
-module.exports = wt.fromExpress(app).auth0({
+export default require('webtask-tools').fromExpress(app).auth0({
   exclude : ['/'],
   loginError: (error, ctx, req, res, baseUrl) => {
-    res.writeHead(401, { 'Content-Type': 'application/json'});
-    res.end(JSON.stringify(err('unauthorized', 401)));
+    res.writeHead(401, { 'Content-Type': 'application/json'})
+    res.end(JSON.stringify({ statusCode: 401, err: 'unauthorized' }))
   }
-});
+})
