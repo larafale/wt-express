e55707fdabf48551da810462bd54cca2d545037c
louis <louis.grellet@gmail.com>
2017-01-22 12:18:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,21 +9,24 @@ app.use((req, res, next) => {
   next()
 })
 
-app.get('/', (req, res) => res.ok({ app: 'running' }))
+app.get('/', (req, res) => res.ok({ app: 'running', access_token: req.query.access_token }))
 
 app.get('/stars', (req, res) => {
   req.webtaskContext.storage.get((err, data) => {
     if(err) return res.err(err)
     data = data || {counter: 1}
     data.counter++
-    req.webtaskContext.storage.set(data, (err) => res.ok(data))
+    req.webtaskContext.storage.set(data, (err) => res.ok(req.user))
   })
 })
 
 export default require('webtask-tools').fromExpress(app).auth0({
-  exclude : ['/'],
+  /*exclude : ['/'],*/
+  scope: 'picture nickname',
   loginError: (error, ctx, req, res, baseUrl) => {
+    res.writeHead(302, { Location: baseUrl+'/login' });
+    return res.end()
     res.writeHead(401, { 'Content-Type': 'application/json'})
-    res.end(JSON.stringify({ statusCode: 401, err: 'unauthorized' }))
+    res.end(JSON.stringify({ statusCode: 401, err: 'unauthorized', baseUrl }))
   }
 })
